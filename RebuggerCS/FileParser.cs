bb0af455d25c26791c67e021ea93df46569a4ec4
wdarvin <wesdarvin@gmail.com>
2016-10-08 13:57:42 -0400

    temp

--- RebuggerCS/FileParser.cs
+++ RebuggerCS/FileParser.cs
@@ -1,10 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
 namespace RebuggerCS
 {
 	public class FileParser
 	{
-		public FileParser()
+		private String[] block;
+		private Dictionary<String, Int32> labels;
+		private Dictionary<String, Object> RO_Data;
+
+		public FileParser(String[] input)
 		{
+			this.block = input;
+		}
+
+		public void GetLabels()
+		{
+			Boolean isText = false;
+			Regex rgx = new Regex(@".*:.*");
+			for (int i = 0; i < block.Length; i++)
+			{
+				if (!isText)
+				{
+					isText = block[i].Trim().Equals(".text");
+				}
+				else
+				{ 
+					if(block[i].)
+				}
+			}
 		}
 	}
 }
--- RebuggerCS/RegisterFile.cs
+++ RebuggerCS/RegisterFile.cs
@@ -15,13 +15,13 @@ namespace RebuggerCS
 
 		public byte GetByte(int register, int position)
 		{
-			if (position > 3) { /*throw new RegisterException(); */}
+			if (position > 3) {throw new RegisterException();}
 			return (byte)(this.data[register] >> (position << 3));
 		}
 
 		public ushort getWord(int register, int position)
 		{
-			if (position > 3) { /*throw new RegisterException(); */}
+			if (position > 3) {throw new RegisterException();}
 			return (ushort)(this.data[register] >> (position << 4));
 		}
 
@@ -32,7 +32,7 @@ namespace RebuggerCS
 
 		public void setByte(int register, int pos, uint value)
 		{
-			if (pos > 3) { /*throw new RegisterException(); */}
+			if (pos > 3) {throw new RegisterException();}
 			uint target = this.data[register];
 			target &= ~((BYTE << 8 * pos));
 			target += (value << (8 * pos));
@@ -41,7 +41,7 @@ namespace RebuggerCS
 
 		public void setWord(int register, int pos, uint value)
 		{
-			if(pos > 2 || pos % 2 != 0){ /*throw new RegisterException(); */}
+			if(pos > 2 || pos % 2 != 0){throw new RegisterException();}
 			uint target = this.data[register];
 			target &= ~((WORD << pos*8));
 			target += (value << (8 * pos));
