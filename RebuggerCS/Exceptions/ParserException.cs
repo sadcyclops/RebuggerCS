78f0a515aabd2f6b6b66c211b1dfea59fa525513
Michael Bayless <michael@yoga>
2016-10-08 14:42:25 -0400

    hi

---
+++ RebuggerCS/Exceptions/ParserException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace RebuggerCS
+{
+	public class ParserException
+	{
+		public ParserException () : base ("There is an error in the syntax of your assembly code") {}
+	}
+}
+
