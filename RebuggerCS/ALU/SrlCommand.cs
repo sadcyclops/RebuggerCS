cc23cae9beb876337ac9f9996af3e21e16ce1c23
Sayeef Moyen <moyen.2@buckeyemail.osu.edu>
2016-10-08 21:09:36 -0400

    All Commands Done

---
+++ RebuggerCS/ALU/SrlCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+namespace RebuggerCS
+{
+	public class SrlCommand : ALUCommand
+	{
+		public SrlCommand(RegisterFile file, RegisterFile special, StackFile stack) : base(file, special, stack) { }
+
+		override public void Execute(List<Int32> entries)
+		{
+			file.SetUInt(entries[0], file.GetUInt(entries[1]) >> entries[2]);
+		}
+
+	}
+}
